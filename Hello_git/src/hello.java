d5c0fad3a54ea01a9b20c41bbc20dd64700757ea
z47 <z47@itcast.cn>
2019-03-14 20:19:28 +0800

    修改Hello_git

--- Hello_git/src/hello.java
+++ Hello_git/src/hello.java
@@ -4,9 +4,11 @@ public class hello {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //hello test
     }
     public void test(){
         int i = 0;
         i+=10;
+        System.out.println(i);
     }
 }
